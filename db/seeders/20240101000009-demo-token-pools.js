5733d2fa24c932dce9ebb91ecc61f48819295a5f
Knox320R <BlazeSkye73@outlook.com>
2025-07-13 20:06:50 -0500

    handle infura test

--- db/seeders/20240101000009-demo-token-pools.js
+++ db/seeders/20240101000009-demo-token-pools.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('token_pools', [
-      { title: 'total_staking', description: 'Total staked amount', amount: 0},
+      { title: 'total_staking', description: 'Total staked amount', amount: 1000000},
       { title: 'daily_staking', description: 'Daily staked amount', amount: 500000},
       { title: 'platform_fee', description: 'Total purchase amount', amount: 100000},
       { title: 'purchase', description: 'Total purchase amount', amount: 0},
